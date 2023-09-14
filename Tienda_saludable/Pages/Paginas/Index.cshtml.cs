bb320befafd8b78a7cba84e04d3444cbf0966082
JUAND4V1D <jc70196106@gmail.com>
2023-09-14 17:23:40 -0400

    ise un hola mundo

---
+++ Tienda_saludable/Pages/Paginas/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tienda_saludable.Pages.Shared.Paginas
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
