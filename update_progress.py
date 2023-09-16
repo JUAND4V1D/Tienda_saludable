ab52b1367b36b292d6c3f55b23c676856bae34d4
JUAND4V1D <143755833+JUAND4V1D@users.noreply.github.com>
2023-09-16 06:18:11 -0400

    update_progress.py

--- update_progress.py
+++ update_progress.py
@@ -1,1 +1,17 @@
+import yaml
+
+# Cargar el archivo de progreso
+with open('progress.yml', 'r') as file:
+    progress = yaml.safe_load(file)
+
+# Calcular el porcentaje total de trabajo
+total_percentage = sum(int(value.strip('%')) for value in progress.values()) / len(progress)
+
+# Generar la barra de progreso en formato Markdown
+progress_bar = f"![Team Progress](https://progress-bar.dev/{total_percentage}/)"
+
+# Actualizar el archivo README.md
+with open('README.md', 'a') as readme:
+    readme.write('\n\n')
+    readme.write(progress_bar)
 
